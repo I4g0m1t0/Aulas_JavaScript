224f498cb190431bc4a88f92c7d7ccbc5a8314a0
Iago <iagorechtramontin@gmail.com>
2024-10-07 16:02:25 -0300

    Aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [0, 4, 8, 9];
+
+num.push(1);
+num.sort();
+
+console.log(num);
+console.log(`O vetor tem ${num.length} posições`);
+console.log(`O primeiro valo do vetor é ${num[0]}`);
+
+let pos = num.indexOf(8);
+console.log(`O valor 8 está na posição ${pos}`);
